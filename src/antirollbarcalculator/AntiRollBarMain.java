10eb3c292942714b7df1ee9fd41327b0ab0bd32c
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2017-03-24 15:50:32 +0000

    First Build

---
+++ src/antirollbarcalculator/AntiRollBarMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antirollbarcalculator;
+
+/**
+ *
+ * @author VladVasiljev
+ */
+public class AntiRollBarMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AntiRollBarGUI antiRollBarGUI = new AntiRollBarGUI();
+        antiRollBarGUI.setVisible(true);
+    }
+    
+}
